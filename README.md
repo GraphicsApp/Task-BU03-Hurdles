d651717b867d586d41dbd028abbb035ec93ebd9a
Alexander Bazo <alexanderbazo@googlemail.com>
2022-10-14 16:07:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Hurdles
+
+Bouncer klettert in diesem Beispielprogramm über drei Hürden. Zu Beginn befindet er am unteren Kartenrand in einem beliebigen Abstand zu dem ersten Hindernis. Zwischen den einzelnen Hindernissen befindet sich jeweils genau ein freies Feld. Die Hindernisse sind ein Feld breit und können beliebig und unterschiedlich hoch sein. Zwischen der Spitze der einzelnen Hindernisse und des oberen Kartenrands muss dabei mindestens ein Feld frei sein. Bouncer klettert über alle Hindernisse und bleibt hinter dem letzten der drei Hindernisse stehen.
+
+
+![Video der erstellten Animation. Bouncer klettert über alle drei Hindernisse und bleibt dann stehen.](docs/demo.gif "Video der erstellen Animation")
+
+## Relevante Bezüge zum OOP-Kurs
+
+Diese Demo kann als erstes Beispiel für ein selbst-geschriebenes Programm verwendet werden. Aufbauend auf einem minimalen Set an Befehlen wird der Startvorgang und die sequenzielle Abarbeitung der einzelnen Befehle innerhalb eines JAVA-Programms demonstriert.
+
+- Struktureller Aufbau und Verwendung eines Bouncer-Programms
+- Verwendung von Schleifen (`for`) zur kontrollierten Wiederholung von Befehlen
+- Verwendung von Schleifen (`while`) zum Erreichen eines gewollten Weltzustands
+- Dokumentation von Vor- und Nachbedingungen für Methoden
---
+++ src/main/java/de/ur/mi/bouncer/hurdles/Hurdles.java
@@ -0,0 +1,95 @@
+package de.ur.mi.bouncer.hurdles;
+
+import de.ur.mi.bouncer.apps.BouncerApp;
+import de.ur.mi.bouncer.apps.BouncerLauncher;
+
+public class Hurdles extends BouncerApp {
+
+    @Override
+    public void bounce() {
+        loadMap("Hurdles");
+        climbHurdles();
+    }
+
+    /**
+     * Bouncer klettert über die drei ersten Hindernisse vor ihm
+     * <p>
+     * Pre-Condition: Bouncer befindet sich am Boden vor dem ersten Hindernis und blickt nach Westen
+     * Post-Condition: Bouncer befindet sich am Boden hinter dem dritten Hindernis und blickt nach Westen
+     */
+    private void climbHurdles() {
+        moveToFirstHurdle();
+        for (int i = 0; i < 3; i++) {
+            jumpOverHurdle();
+        }
+    }
+
+    /**
+     * Bouncer bewegt sich zum ersten Hindernis
+     * <p>
+     * Pre-Condition: Bouncer befindet sich beliebig viele Felder vor dem ersten Hindernis
+     * Post-Condition: Bouncer befindet sich direkt vor dem ersten Hindernis
+     */
+    private void moveToFirstHurdle() {
+        while (bouncer.canMoveForward()) {
+            bouncer.move();
+        }
+    }
+
+    /**
+     * Bouncer übersteigt ein einzelnes Hindernis
+     * <p>
+     * Pre-Condition: Bouncer befindet sich direkt vor dem Hindernis, steht an dessen unterem Ende und blickt nach Westen
+     * Post-Condition: Bouncer befindet sich direkt hinter dem Hindernis, steht an dessen unterem Ende und blickt nach Westen
+     */
+    private void jumpOverHurdle() {
+        ascendHurdle();
+        bouncer.move();
+        descendHurdle();
+    }
+
+    /**
+     * Bouncer klettert auf das vor ihm liegende Hindernis
+     * <p>
+     * Pre-Condition: Bouncer befindet sich direkt vor dem Hindernis, steht an dessen unterem Ende und blickt nach Westen
+     * Post-Condition: Bouncer befindet sich auf der Spitze des Hindernis und blickt nach Westen
+     */
+    private void ascendHurdle() {
+        bouncer.turnLeft();
+        while (bouncer.canNotMoveRight()) {
+            bouncer.move();
+        }
+        turnRight();
+        bouncer.move();
+    }
+
+    /**
+     * Bouncer klettert das vor ihm liegende Hindernis herab
+     * <p>
+     * Pre-Condition: Bouncer befindet sich auf der Spitze des Hindernisses und blickt nach Westen
+     * Post-Condition: Bouncer befindet sich direkt am Fuße des überwundenen Hindernisses und blickt nach Westen
+     */
+    private void descendHurdle() {
+        turnRight();
+        while (bouncer.canMoveForward()) {
+            bouncer.move();
+        }
+        bouncer.turnLeft();
+    }
+
+    /**
+     * Bouncer dreht sich um 90 Grad nach rechts
+     * <p>
+     * Pre-Condition: -
+     * Post-Condition: Bouncers Blickrichtung hat sich um 90 Grad nach Rechts verschoben
+     */
+    private void turnRight() {
+        bouncer.turnLeft();
+        bouncer.turnLeft();
+        bouncer.turnLeft();
+    }
+
+    public static void main(String[] args) {
+        BouncerLauncher.launch();
+    }
+}
